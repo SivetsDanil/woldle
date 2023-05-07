2d12ee31ef28cb337c37e0ebba656704df414564
SivetsDanil <103943389+SivetsDanil@users.noreply.github.com>
2023-05-07 19:15:18 +0300

    С настройками всё

--- Вордли/main.py
+++ Вордли/main.py
@@ -34,7 +34,7 @@ def main():
     return json.dumps(response)
 
 
-def make_response(text=None, card=None, tts=None, buttons=[], end=False, user={}):
+def make_response(text="null", card=None, tts=None, buttons=[], end=False, user={}):
     json.dump(user, open(f'{user["id"]}.json', 'w', encoding='utf8'), indent=4, ensure_ascii=False)
     response = {
         "response": {
@@ -66,7 +66,7 @@ def handler(event, e):
             user_dict = json.load(open(f'{user}.json', encoding='utf8'))
         except:
             user_dict = {"id": user, "name": "", "strike": 0, "old_words": [], "action": 'name', "word": "",
-                         "Counter": 0, "language": "rus", "lange": 5, "level": "beginner", "change_action": ''}
+                         "Counter": 0, "language": "русский", "lange": "5", "level": "начинающий", "change_action": ''}
             text = 'Привет! Давай знакомиться, меня зовут Вордл, а тебя?'
             return make_response(text=text, user=user_dict)
         if json.load(open(f'{user}.json', encoding='utf8'))["action"] == 'name':
@@ -92,40 +92,58 @@ def handler(event, e):
             user_dict["change_action"] = 'change_lange'
             return make_response(text='Скажи, какой длины от 3 до 6 букв давать слова?', user=user_dict)
         elif user_dict["change_action"] == 'change_lange':
-            user_dict["action"] = 'settings'
-            if "три" in user_request:
+            if "три" in user_request or "3" in user_request:
                 user_dict["lange"] = 3
-            elif "чет" in user_request:
+            elif "чет" in user_request or "4" in user_request:
                 user_dict["lange"] = 4
-            elif "пят" in user_request:
+            elif "пят" in user_request or "5" in user_request:
                 user_dict["lange"] = 5
-            elif "шест" in user_request:
+            elif "шест" in user_request or "6" in user_request:
                 user_dict["lange"] = 6
             else:
-                return make_response(text=f'Я тебя не понял, пожалуйста, сказажи иначе...', user=user_dict)
+                return make_response(text=f'Я тебя не понял, пожалуйста, сказажи иначе...\nНа всякий случай, я даю слова от 3 до 6 букв', user=user_dict)
+            user_dict["action"] = 'settings'
             user_dict["change_action"] = ""
             return make_response(text=f'Теперь буду загадывать слова по {user_request} букв.', user=user_dict)
         elif user_request == 'смена сложности':
             user_dict["change_action"] = 'change_level'
-            return make_response(text='Скажи мне, какой сложности загадывать слова?\nВыбор из: "Начинающий", "Игрок wordle" и "Эрудита"', user=user_dict)
+            card = {
+                "type": "ItemsList",
+                "header": {
+                    "text": "Выбери подходящий уровень сложности"
+                },
+                "items": levels,
+            }
+            buttons = [{"title": "Выйти", "hide": False}]
+            return make_response(card=card, user=user_dict, buttons=buttons)
         elif user_dict["change_action"] == 'change_level':
+            if user_request not in ['начинающий', 'продвинутый', 'эрудит']:
+                return make_response(text='Пожалуйста, выбери один из представленных или используй кнопки..', user=user_dict)
             user_dict["action"] = 'settings'
             user_dict["level"] = user_request
             user_dict["change_action"] = ""
-            return make_response(text=f'Теперь буду загадывать слова для уровня {user_request}.', user=user_dict)
+            return make_response(text=f'Теперь буду загадывать слова для уровня "{user_request.capitalize()}".', user=user_dict)
         elif user_request == 'смена языка':
             user_dict["change_action"] = 'change_language'
             return make_response(text='Какой язык выберешь: русский или английский?', user=user_dict)
         elif user_dict["change_action"] == 'change_language':
-            user_dict["action"] = 'settings'
-            user_dict["language"] = user_request
-            user_dict["change_action"] = ""
-            return make_response(text=f'Теперь буду загадывать слова из языка {user_request}.', user=user_dict)
+            if 'рус' in user_request or "rus" in user_request:
+                user_dict["language"] = 'русский'
+                user_dict["action"] = 'settings'
+                user_dict["change_action"] = ""
+                return make_response(text=f'Теперь буду загадывать слова из русского языка.', user=user_dict)
+            elif "анг" in user_request or "eng" in user_request:
+                user_dict["language"] = 'английский'
+                user_dict["action"] = 'settings'
+                user_dict["change_action"] = ""
+                return make_response(text=f'Теперь буду загадывать слова из английского языка.', user=user_dict)
+            else:
+                return make_response(text=f'Я тебя не понял, пожалуйста, сказажи иначе...', user=user_dict)
         else:
             user_dict["action"] = 'settings'
     if user_request == 'настройки' or user_dict["action"] == "settings":
         if user_dict["name"] == '':
-            return make_response(text=f'Скажи свое имя, незнакомцам с настройками не помогаю...', user=user_dict)
+            return make_response(text=f'Пожалуйста, скажи свое имя, а то незнакомцам я с настройками не помогаю...', user=user_dict)
         settings = [
             {
                 "image_id": "1652229/3d093b89fdaebbf80b62",
@@ -160,9 +178,8 @@ def handler(event, e):
             },
             "items": settings,
         }
-        text = "Ты в настройках, выбери что хочешь изменить."
         buttons = [{"title": "Выйти", "hide": False}]
-        return make_response(text=text, card=card, user=user_dict, buttons=buttons)
+        return make_response(card=card, user=user_dict, buttons=buttons)
     else:
         if user_dict["action"] == 'name':
             user_dict['name'] = user_request.capitalize()
