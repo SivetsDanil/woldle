535f38ba7692267700b5e08f5512f2a7595d3a8f
IceEnderGaming <karta8950@gmail.com>
2023-05-20 07:24:56 +0700

    Слова 50%

--- Вордли/test.py
+++ Вордли/test.py
@@ -1,5 +1,10 @@
-f = open("words.txt", "r").readlines()
+import texts
+k = {'sun', 'box', 'fax', 'oil', 'fox', 'cat', 'bad', 'tee', 'car', 'die', 'pig', 'ice', 'big', 'bee', 'one', 'try', 'bus', 'two', 'end', 'jam', 'ski', 'cow', 'pet', 'aid', 'can', 'joy', 'men', 'key', 'guy', 'fog', 'pie', 'bit', 'axe', 'pin', 'job', 'fit', 'air', 'fix', 'cap', 'arm', 'eye', 'bar', 'fun', 'kid', 'mum', 'dog', 'boy', 'toy', 'egg', 'sky'}
+s = []
+while len(s) != 50:
+  x = input()
+  if len(x) == 3 and x not in s and x in texts.eng_play_words[3] and x not in k:
+    s.append(x)
+    print("complete!")
 
-
-
-print({i.strip().split(" ")[-1].lower() for i in f if len(i.strip())})
+print(set(s))
