c888235a3a68a1dd7f335efdba1326c9853f2dd1
SivetsDanil <103943389+SivetsDanil@users.noreply.github.com>
2023-05-26 15:06:14 +0300

    система топа

--- Вордли/test.py
+++ Вордли/test.py
@@ -1,8 +1,5 @@
 import csv
-try:
-    users_top = list(csv.DictReader(open("users/1_users_top.csv", "r"), delimiter=';'))
-    users_top.sort(key=lambda x: -int(x["exp"]))
-    for r in users_top:
-        print(r)
-except Exception as e:
-    print(e)
+users_top = list(csv.DictReader(open("users/1_users_top.csv", "r"), delimiter=';'))
+users_top.sort(key=lambda x: -int(x["exp"]))
+for r in users_top:
+    print(r)
